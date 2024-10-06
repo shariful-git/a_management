c8ca1bfaf3d996df3b4adbde2b289624a8fd6b04
shariful-git <mdsharifulkawsar@gmail.com>
2024-10-06 15:21:09 +0600

    update bugs code

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SettingController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -17,25 +18,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('auth.login');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/settings', [SettingController::class, 'index']);
-Route::get('/contact', [ContactController::class, 'contact']);
+// home
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 // contact
+Route::get('/contact', [ContactController::class, 'contact']);
 Route::post('/submitContact', [ContactController::class, 'submit'])->name('contact');
 
 // Profile
 Route::post('/update_profile_informations', [ContactController::class, 'updateProfileInfo']);
 
-//post
+// post
 Route::post('/add/article/post', [ArticleController::class, 'store'])->name('add-article-post');
 
-
+// setting
+Route::get('/settings', [SettingController::class, 'index']);
+Route::delete('/delete/me', [SettingController::class, 'delete'])->name('deleteMe');
 
 
